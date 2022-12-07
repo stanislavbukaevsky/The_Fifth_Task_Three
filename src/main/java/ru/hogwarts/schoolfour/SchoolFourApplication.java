2134643afce41ef452b4402b5df9e4f05a9fa08e
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-12-07 13:57:10 +0300

    Первый коммит

---
+++ src/main/java/ru/hogwarts/schoolfour/SchoolFourApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolFourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/schoolfour/SchoolFourApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schoolfour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolFourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
